80f04ef86522e163554a375562759f9fa941327f
Elizabeth ORiordan <elizabeth.oriordan@flatironschool.com>
2020-02-24 00:19:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,10 @@
 def reverse_each_word(s)
   a = s.split(" ")
-  return a.map{|i|: i.reverse}
+  a.collect{|i|: i.reverse}
+end
+
+def test(sentence)
+ array = []
+ array << sentence.split
+ array.collect {|word| word.reverse}
 end
